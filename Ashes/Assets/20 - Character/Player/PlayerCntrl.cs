da1185863b3b9eb3c7197ac36877abb6288fd19a
upallnightcoding <upallnightcoding@gmail.com>
2024-10-04 00:21:55 -0400

    Implement Controller

--- Ashes/Assets/20 - Character/Player/PlayerCntrl.cs
+++ Ashes/Assets/20 - Character/Player/PlayerCntrl.cs
@@ -7,6 +7,8 @@ public class PlayerCntrl : MonoBehaviour
 {
     private Vector2 leftControl, rightControl;
 
+    private float speed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,24 +23,48 @@ public class PlayerCntrl : MonoBehaviour
 
     private void MovePlayer(float dt)
     {
-        if (leftControl != Vector2.zero)
+        if (leftControl.magnitude > 0.15f)
         {
-
+            Vector3 direction = new Vector3(leftControl.x, 0.0f, leftControl.y).normalized;
+            transform.Translate(speed * direction * Time.deltaTime, Space.World);
+            Quaternion rotTarget = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rotTarget, 500.0f * Time.deltaTime);
         }
     }
 
-    private void OnMove(InputAction.CallbackContext context)
+    public void OnMove(InputAction.CallbackContext context)
     {
-        if (context.started)
+        if (context.performed)
         {
             leftControl = context.ReadValue<Vector2>();
-            Debug.Log($"Left Controller (Started): {leftControl}");
+            //Debug.Log($"Left Controller (Started): {leftControl}");
+        }
+
+        /*if (context.canceled)
+        {
+            leftControl = Vector2.zero;
+            Debug.Log($"Left Controller (Canceled): {leftControl}");
+        }*/
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            //Debug.Log($"Mouse Input: {context.ReadValue<Vector2>()}");
+        }
+    }
+
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log($"OnFire Start ...");
         }
 
         if (context.canceled)
         {
-            leftControl = Vector2.zero;
-            Debug.Log($"Left Controller (Canceled): {leftControl}");
+            Debug.Log($"OnFire End ...");
         }
     }
 }
