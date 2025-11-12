76b1e67a4947de2aea84bcad1f1bb684a901bc96
upallnightcoding <upallnightcoding@gmail.com>
2025-11-12 02:28:42 -0500

    Jump Player Control

--- Ashes/Assets/40 - Characters/Player/PlayerCntrl.cs
+++ Ashes/Assets/40 - Characters/Player/PlayerCntrl.cs
@@ -1,65 +1,190 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerCntrl : MonoBehaviour
+namespace Ashes.PlayerCntrl
 {
-    // Character Movement Controls
-    //----------------------------
-    private Vector2 playerMovement;
-    private Vector3 moveDirection;
-    private float playerSpeed = 8.0f;
-    private float playerRotation = 400.0f;
-    private CharacterController charCntrl;
-
-    // Animation Controls
-    //-------------------
-    private Animator animator;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PlayerCntrl : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-        charCntrl = GetComponent<CharacterController>();
-    }
+        // Character Movement Controls
+        //----------------------------
+        [Header("Character Movement Controls")]
+        [SerializeField] private float playerSpeed = 8.0f;
+        [SerializeField] private float playerRotation = 400.0f;
+        [SerializeField] private float GRAVITY = -9.81f;
+        [SerializeField] private float gravityMultiplier = 3.0f;
+        private Vector2 playerMovement;
+        private Vector3 moveDirection;
+        private CharacterController charCntrl;
+        private float jumpHeight = 2.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        moveDirection.x = playerMovement.x; // Horizontal Axis Controls
-        moveDirection.y = 0.0f;
-        moveDirection.z = playerMovement.y; // Vertical Axis Controls
+        // Animation Controls
+        //-------------------
+        private Animator animator;
 
-        if (moveDirection != Vector3.zero)
+        private PlayerState currentState = PlayerState.IDLE;
+
+        private PlayerInputCntrl playerInputCntrl = PlayerInputCntrl.DO_NOTHING;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
         {
-            animator.SetBool("run", true);
+            animator = GetComponent<Animator>();
+            charCntrl = GetComponent<CharacterController>();
+        }
 
-            moveDirection.Normalize();
+        void Update()
+        {
+            switch (currentState)
+            {
+                case PlayerState.IDLE:
+                    currentState = State_Idle(playerInputCntrl);
+                    break;
+                case PlayerState.RUNNING:
+                    currentState = State_Running(playerInputCntrl);
+                    break;
+                case PlayerState.JUMP:
+                    currentState = State_Jump();
+                    break;
+                case PlayerState.JUMPING:
+                    currentState = State_Jumping(moveDirection);
+                    break;
+            }
+        }
 
-            float inputMagnitude = Mathf.Clamp01(moveDirection.magnitude);
+        #region *** Finite State Machine ***
 
-            Vector3 velocity = inputMagnitude * playerSpeed * moveDirection;
+        private PlayerState State_Jump()
+        {
+            jumpHeight = 3.0f;
 
-            //navMeshAgent.Move(velocity * Time.deltaTime);
-            charCntrl.Move(velocity * Time.deltaTime);
+            moveDirection.y += jumpHeight;
 
+            MovePlayer(moveDirection);
+
+            return (PlayerState.JUMPING);
+        }
+
+        private PlayerState State_Jumping(Vector3 moveDirection)
+        {
+            MovePlayer(moveDirection);
+
+            moveDirection.y += GRAVITY * Time.deltaTime;
+
+            Debug.Log($"moveDirection.y: {moveDirection.y}");
+
+            return (moveDirection.y > 0.0f ? PlayerState.JUMPING : PlayerState.RUNNING);
+        }
+
+        private PlayerState State_Idle(PlayerInputCntrl playerInput)
+        {
+            PlayerState nextState = PlayerState.IDLE;
+
+            if (playerInput == PlayerInputCntrl.MOVEMENT)
+            {
+                nextState = PlayerState.RUNNING;
+            }
+            else
+            {
+                animator.SetBool("run", false);
+            }
+
+            return (nextState);
+        }
+
+        private PlayerState State_Running(PlayerInputCntrl playerInputCntrl)
+        {
+            PlayerState nextState = PlayerState.RUNNING;
+
+            switch (playerInputCntrl)
+            {
+                case PlayerInputCntrl.MOVEMENT:
+                    RotatePlayer(moveDirection);
+                    MovePlayer(moveDirection);
+                    break;
+                case PlayerInputCntrl.JUMP:
+                    nextState = PlayerState.JUMP;
+                    break;
+                default:
+                    nextState = PlayerState.IDLE;
+                    break;
+            }
+
+            return (nextState);
+        }
+
+        #endregion
+
+        #region *** Finite State Machine Support Functions ***
+
+        /**
+         * RotatePlayer() - 
+         */
+        private void RotatePlayer(Vector3 moveDirection)
+        {
             Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, playerRotation * Time.deltaTime);
         }
-        else
+
+        /**
+         * MovePlayer() - 
+         */
+        private void MovePlayer(Vector3 moveDirection)
         {
-            animator.SetBool("run", false);
+            animator.SetBool("run", true);
+
+            charCntrl.Move(playerSpeed * moveDirection * Time.deltaTime);
         }
+
+        #endregion
+
+        #region *** Player Movement Functions ***
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            playerMovement = context.ReadValue<Vector2>();
+
+            if (playerMovement != Vector2.zero)
+            {
+                playerInputCntrl = PlayerInputCntrl.MOVEMENT;
+
+                moveDirection.x = playerMovement.x; // Horizontal Axis Controls
+                moveDirection.y = 0.0f;
+                moveDirection.z = playerMovement.y; // Vertical Axis Controls
+            }
+            else
+            {
+                playerInputCntrl = PlayerInputCntrl.DO_NOTHING;
+            }
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                Debug.Log($"OnJump ...");
+
+                playerInputCntrl = PlayerInputCntrl.JUMP;
+            }
+        }
+
+        #endregion
     }
 
-    #region *** Player Movement Functions ***
-
-    public void OnMove(InputAction.CallbackContext context)
+    public enum PlayerState
     {
-        //Debug.Log($"OnMove: {context.ReadValue<Vector2>()}");
-
-        playerMovement = context.ReadValue<Vector2>();
+        IDLE,
+        RUNNING,
+        JUMPING,
+        JUMP
     }
 
-    #endregion
+    public enum PlayerInputCntrl
+    {
+        DO_NOTHING,
+        MOVEMENT,
+        JUMP
+    }
 }
+
+
